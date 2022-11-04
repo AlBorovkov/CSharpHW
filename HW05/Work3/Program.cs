dac4c999d07778c53b9e80d9090b7a17e7576e01
Borovkov Alexander <kekushf@gmail.com>
2022-11-04 19:58:33 +0300

    Task38

---
+++ HW05/Work3/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 38: 
+// Задайте массив вещественных чисел. 
+// Найдите разницу между максимальным и минимальным элементов массива.
+// [3 7 22 2 78] -> 76
+
+void FillArray(double[] newArray)
+{
+    Console.Write("Массив: ");
+    for (int i = 0; i < newArray.Length; i++)
+    {
+        newArray[i] = new Random().Next(-99, 100);
+        Console.Write($"{newArray[i]} ");
+    }
+    Console.WriteLine();
+
+    double max = newArray[0];
+    double min = newArray[0];
+    for (int i = 0; i < newArray.Length; i++)
+    {
+        if(newArray[i] > max) max = newArray[i];
+        if(newArray[i] < min) min = newArray[i];
+    }
+    Console.WriteLine($"Разница между максимальным ({max}) и минимальным ({min}) элементами массива равна {max - min}");
+}
+
+double[] array = new double[10];
+FillArray(array);
