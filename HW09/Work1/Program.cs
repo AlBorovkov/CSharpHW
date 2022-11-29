9629946e4b641448a1e381ea190604c032dd958a
Borovkov Alexander <kekushf@gmail.com>
2022-11-29 03:06:22 +0300

    Task 64

---
+++ HW09/Work1/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача 64: 
+//  Задайте значение N. Напишите программу, которая выведет все чётные числа в промежутке от N до 1.
+//  Выполнить с помощью рекурсии.
+//  N = 5 -> "4, 2"
+//  N = 8 -> "8, 6, 4, 2,"
+
+
+void Main()
+{
+    Console.Clear();
+    Console.WriteLine("Введите число N");
+    int n = int.Parse(Console.ReadLine()!);
+    EvenNumbers(n);
+}
+
+void EvenNumbers(int n)
+{
+    if (n <= 1) return;
+    else
+    {
+        if (n % 2 == 0)
+        {
+            Console.Write($" {n}");
+            EvenNumbers(n - 2);
+        }
+        else
+        {
+            n--;
+            Console.Write($" {n}");
+            EvenNumbers(n - 2);
+        }
+    }
+}
+Main();
